c01412104b489e9d4f953bade460d2a0e3a7dea1
lnn0q <denysenko.e0@gmail.com>
2024-06-04 02:27:04 +0300

    Limmiters adjusted for images

--- src/server/main.js
+++ src/server/main.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 
 app.use("/api/template", mailTemplateRouter);
 app.use("/api/export-eml", emlExportRouter);
